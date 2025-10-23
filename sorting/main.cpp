0dcaa08bc61b18256ca7129f7d55362543f99540
Lucas Sena <lucasmelosena@gmail.com>
2025-10-22 21:58:26 -0300

    add quick sort code

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -112,6 +112,33 @@ void mergeSort(int array[], int leftIndex, int rightIndex) {
   merge(array, leftIndex, midIndex, rightIndex);
 }
 
+void swap(int array[], int firstIndex, int secondIndex) {
+  int temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+int pivot(int array[], int pivotIndex, int endIndex) {
+  int swapIndex = pivotIndex;
+  for (int i = pivotIndex + 1; i <= endIndex; i++) {
+    if (array[i] < array[pivotIndex]) {
+      swapIndex++;
+      swap(array, swapIndex, i);
+    }
+  }
+  swap(array, pivotIndex, swapIndex);
+  return swapIndex;
+}
+
+void quickSort(int array[], int leftIndex, int rightIndex) {
+  if (leftIndex >= rightIndex)
+    return;
+
+  int pivotIndex = pivot(array, leftIndex, rightIndex);
+  quickSort(array, leftIndex, pivotIndex - 1);
+  quickSort(array, pivotIndex + 1, rightIndex);
+}
+
 int main()
 {
   int myArr[] = {1,3,7,8,2,4,5,6};
@@ -123,8 +150,9 @@ int main()
   // bubbleSort(myArr, size);
   // selectionSort(myArr, size);
   // insertionSort(myArr, size);
-  mergeSort(myArr, leftIndex, rightIndex);
-  
+  // mergeSort(myArr, leftIndex, rightIndex);
+  quickSort(myArr, leftIndex, rightIndex);
+
   for (auto num : myArr)
   {
     cout << num << " ";
